53d19d8b8d2811b74a6fff70b2e5fd90800f860f
BielVereda <gabrielsantosvereda@gmail.com>
2026-09-15 10:12:15 -0300

    Adicionar ícones e refatorar a seção de categorias para usar ScrollView

--- src/components/CategorySection/index.js
+++ src/components/CategorySection/index.js
@@ -1,19 +1,22 @@
-import type { Container } from "../../screens/Home/style"
-import { CardCategory } from "../CardCategory"
-import { TextCategory, ContainerCategory, TextHighlight, TextTitle } from "./style"
+import { ScrollView, Text, View } from "react-native"
+import { ContainerCategory, Scroll, TextHighlight, TextTitle } from "./style"
+import { CardCategory } from "./CardCategory"
+import iconTenis from '../../../assets/tenisIcon.png'
+import iconEletronicos from '../../../assets/eletricosIcon.png'
+import iconCamisetas from '../../../assets/camisetasIcon.png'
+import iconJogos from '../../../assets/jogosIcon.png'
 
 export const CategorySection = () => {
     return(
         <ContainerCategory>
-            <TextTitle>Nossas<TextHighlight>Categorias</TextHighlight></TextTitle>
-
-            <View>
-                <CardCategory image={} name="Tênis"/>
-                <CardCategory image={} name="Eletrônicos"/>
-                <CardCategory image={} name="Camisetas"/>
-                <CardCategory image={} name="Jogos"/>
-                <CardCategory image={} name="Eletrodomésticos"/>
-            </View>
+            <TextTitle>Nossas <TextHighlight>Categorias</TextHighlight></TextTitle>
+            
+            <Scroll horizontal showsHorizontalScrollIndicator={false}>
+                <CardCategory image={iconTenis} name="Tênis"/>
+                <CardCategory image={iconEletronicos} name="Eletrônicos"/>
+                <CardCategory image={iconCamisetas} name="Camisetas"/>
+                <CardCategory image={iconJogos} name="Jogos"/>
+            </Scroll>
 
         </ContainerCategory>
     )
